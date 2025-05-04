c4683abcecc1107b1c5d4635d5bb597e46b5d8b0
Bryan Navarrete <bryanhenrry94@gmail.com>
2025-05-04 11:55:16 -0500

    tailwindcss configuration file

--- src/middleware.ts
+++ src/middleware.ts
@@ -48,5 +48,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next|favicon.ico|api).*)"], // Excluye rutas estáticas y API
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
